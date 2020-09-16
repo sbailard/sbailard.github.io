87d1b8150fc58cc3c6353431e28380a940f2f53c
sbailard <bailard.s@northeastern.edu>
2020-09-15 22:35:37 -0400

    Move files to my git  website

---
+++ scripts/main.js
@@ -0,0 +1,36 @@
+
+/** This code is based off of Mozilla's Tutoials: https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web/JavaScript_basics */
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/firefox-icon.png') {
+      myImage.setAttribute('src','images/firefox2.png');
+    } else {
+      myImage.setAttribute('src','images/firefox-icon.png');
+      
+    }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt('Enter your name: ');
+    if(!myName) {
+      setUserName();
+    } else {
+      localStorage.setItem('name', myName);
+      myHeading.innerHTML = 'Welcome ' + myName + '!';
+    }
+  }
+
+  if(!localStorage.getItem('name')) {
+    setUserName();
+  } else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Welcome ' + storedName + '!';
+  }
+  myButton.onclick = function() {
+    setUserName();
+  }
